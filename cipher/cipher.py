63bed7ecc23b631e475067ef9ff78bd700eb0940
40sherrin <fareedaskari42@gmail.com>
2021-03-02 19:35:38 +1100

    Cipher Function

--- cipher/cipher.py
+++ cipher/cipher.py
@@ -1,1 +1,13 @@
 # A simple cipher function
+letter = input("Word: ")
+key = input("Key: ")
+
+
+def rotate(l, k):
+    new = ord(letter) + key
+    new = chr(new)
+    return new
+
+
+encrypt = rotate(letter, key)
+print(encrypt)
