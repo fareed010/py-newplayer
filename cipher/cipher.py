c3a089715dd1c8e3ec791be7e38c69bfa0b49d81
40sherrin <fareedaskari42@gmail.com>
2021-03-02 20:02:45 +1100

    Cipher Error handling

--- cipher/cipher.py
+++ cipher/cipher.py
@@ -4,9 +4,10 @@ key = int(input("Key: "))
 
 
 def rotate(l, k):
-    new = ord(l) + k
-    new = chr(new)
-    return new
+    encrypted = ''
+    for char in l:
+        encrypted += ord(char) + k
+    return encrypted
 
 
 encrypt = rotate(letter, key)
