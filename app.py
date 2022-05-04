199116bf5efc9efe794c22dd847c7a3ec845db3f
40sherrin <fareedaskari42@gmail.com>
2022-05-04 21:22:01 +1000

    set and methods

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-class PrintColor:
-    red_color = "\033[91m {}\033[00m"
-    green_color = "\033[92m {}\033[00m"
+thisset = {"apple", "banana", "cherry"}
 
-    def __init__(self, text):
-        self.text = text
-
-    def print_red(self):
-        print(self.red_color.format(self.text))
-
-    def print_green(self):
-        print(self.green_color.format(self.text))
-
-prnt = PrintColor("Text to be printed")
-
-prnt.print_green()
-prnt.print_red()
+print(len(thisset))
